4010fa58f8b33ed882bd54435d1260ea185ad0c0
あっつん <daisuke.samuser@gmail.com>
2016-11-04 09:43:38 +0000

    first commit

--- app/controllers/sessions_controller.rsessions_controller.rbsessions_controller.rb
+++ app/controllers/sessions_controller.rsessions_controller.rbsessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+    
+ # 渡されたユーザーでログインする
+ def log_in(user)
+  session[:user_id] = user.id
+ end
+ 
+ # 現在ログインしているユーザーを返す（ユーザーがログイン中の場合のみ）
+ def current_user
+  @current_user ||= User.find_by(id:session[:user_id])
+ end
 end
