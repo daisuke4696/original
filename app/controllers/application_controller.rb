80962e82ee62d54a2cbc1d7de2f34ebeae40cb76
あっつん <daisuke.samuser@gmail.com>
2016-11-16 07:36:02 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 end
+
+
+private
+
+def set_main_content_bg_white_color
+  @main_content_bg_color = Settings.main_content_bg_color.white
+end
